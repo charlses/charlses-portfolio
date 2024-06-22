adfb5a081bc3349bc898b540a83f78f10a15eb6f
Davit Tavadze <davit@techpheonix.dev>
2024-06-22 22:25:03 +0400

    updated metadata and adjusted the footer for the light mode

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,10 @@ import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <footer className='w-full pt-20 pb-10 bg-black-100 border-t' id='contact'>
+    <footer
+      className='w-full pt-20 pb-10 dark:bg-black-100 bg-white border-t'
+      id='contact'
+    >
       {/* background grid */}
       <div className='w-full absolute left-0 -bottom-72 min-h-96'>
         <img
