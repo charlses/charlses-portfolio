8c163eab71f281fe45e115c17d739898efe7b342
Davit Tavadze <davit@techpheonix.dev>
2024-06-22 22:37:00 +0400

    removed footer grid

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,18 +8,9 @@ import Link from 'next/link'
 const Footer = () => {
   return (
     <footer
-      className='w-full pt-20 pb-10 dark:bg-black-100 bg-white border-t'
+      className='w-full pt-20 pb-10 dark:bg-black-100 bg-white border-t bg-grid'
       id='contact'
     >
-      {/* background grid */}
-      <div className='w-full absolute left-0 -bottom-72 min-h-96'>
-        <img
-          src='/footer-grid.svg'
-          alt='grid'
-          className='w-full h-full opacity-50 '
-        />
-      </div>
-
       <div className='flex flex-col items-center'>
         <h1 className='heading lg:max-w-[45vw]'>
           Ready to take <span className='text-purple'>your</span> digital
