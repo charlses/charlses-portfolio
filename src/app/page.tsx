15a506c599255dd82010cd9e93c8c0db28cd70d7
Davit Tavadze <davit@techpheonix.dev>
2024-06-22 14:03:21 +0400

    added light mode for hero and about sections.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import RecentProjects from '@/components/RecentProjects'
 
 export default function Home() {
   return (
-    <main className='relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5'>
+    <main className='relative dark:bg-black-100  bg-white flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5'>
       <div className='max-w-7xl w-full'>
         <Hero />
         <Grid />
